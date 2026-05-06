b0b554538cf9d0e8c6dc6e566d0618b20688949e
ChubYf <13425235397@163.com>
2026-05-06 11:10:40 +0800

    firstblog

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,13 +7,13 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "QWQ",
+	title: "Chuby's blog",
 
 	// 站点副标题
 	subtitle: "AWA",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.213188.xyz",
 
 	// 站点描述
 	description:
---
+++ src/content/firstblog.md
@@ -0,0 +1,14 @@
+---
+title: Chuby's blog
+published: 2026-05-06
+description: 我的第一篇博客
+image: ./cover.jpg
+tags: [杂记]
+category: 杂记
+draft: false
+---
+
+
+# test
+111
+
